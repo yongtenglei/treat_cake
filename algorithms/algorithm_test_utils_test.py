d82db3999bb86183a83c354433cb5ec746b63ead
yongtenglei <yongtengrey@outlook.com>
2024-06-26 21:15:41 +0100

    fix

--- algorithms/algorithm_test_utils_test.py
+++ algorithms/algorithm_test_utils_test.py
@@ -7,8 +7,10 @@ def test_all_allocations():
     cuts = [0.25, 0.5, 0.75]
     num_agents = 4
 
-    expected_number_of_allocations = len(
-        list(permutations(range(len(cuts) + 1), num_agents))
-    )
-
+    generated_allocations = [
+        allocation for allocation in generate_all_possible_allocations(cuts, num_agents)
     ]
+    # number of permutation = n! where n = 4
+    expected_number_of_allocations = 24
+    print(generated_allocations)
+    assert len(generated_allocations) == expected_number_of_allocations
