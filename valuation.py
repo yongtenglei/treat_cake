c4c543d31e6693a23a334f939890ad508e8868a4
yongtenglei <yongtengrey@outlook.com>
2024-06-26 16:31:10 +0100

    revised double prime for one segmentation

--- valuation.py
+++ valuation.py
@@ -5,9 +5,9 @@ from treat_cake.base_types import Segment
 from treat_cake.values_bak import get_value_for_interval
 
 
-def _v(segments: List[Segment], start: float, end: float) -> float:
-    v = get_value_for_interval(segments, start, end)
-    print(f"v={v}")
+def _v(segments: List[Segment], a: float, b: float) -> float:
+    v = get_value_for_interval(segments, a, b)
+    print(f"v={v}({a=}, {b=})")
     return v
 
 
@@ -20,13 +20,66 @@ def _v_prime(
     # v is the original valuation function
     # v_prime = base_value / 2 + perturbation
     v = _v(segments, a, b) / 2 + epsilon * abs(b - a)
-    print(f"v_prime={v}")
+    print(f"v_prime={v}({a=}, {b=})")
     return v
 
 
-def v_double_prime_for_interval(
-    segments: List[Segment], delta: float, a: float, b: float
+# def get_double_prime_for_interval(
+#     segments: List[Segment], epsilon: float, start: float, end: float
+# ) -> float:
+#     """
+#     Returns the total double prime value of an interval,
+#     even if covers several segments or splits segments in half.
+#     """
+#     total = 0
+#     for seg in segments:
+#         if seg.end <= start or seg.start >= end:
+#             # this segment not relevant
+#             continue
+#
+#         total += _v_double_prime(seg, epsilon, start, end)
+#     return total
+
+
+def get_double_prime_for_interval(
+    segments: List[Segment], epsilon: float, start: float, end: float
 ) -> float:
+    """
+    Returns the total double prime value of an interval,
+    even if covers several segments or splits segments in half.
+    """
+    assert 0 <= start <= end, "start or end out of range"
+
+    total = 0
+
+    start_int = int(start)
+    end_int = int(end)
+
+    # Only one segment
+    if end <= 1:
+        return _v_double_prime(segments, epsilon, start, end)
+
+    if start == start_int and end == end_int:
+        return _v_double_prime(segments, epsilon, start, end)
+
+    # Start segments
+    if start != start_int:
+        first_segment_end = start_int + 1
+        total += _v_double_prime(segments, epsilon, start, first_segment_end)
+
+    # Intermedia segments
+    if start_int + 1 < end_int:
+        total += _v_double_prime(segments, epsilon, start_int + 1, end_int)
+
+    # Last segments
+    if end != end_int:
+        last_segment_start = end_int
+        total += _v_double_prime(segments, epsilon, last_segment_start, end)
+
+    return total
+
+
+def _v_double_prime(segments: List[Segment], delta: float, a: float, b: float) -> float:
 
     # Letting delta := epsilon, so,
     # any epsilon-envy-free allocation for (v_double_prime) is 5*epsilon-envy-free for (v_prime) for each agent.
@@ -36,7 +89,7 @@ def v_double_prime_for_interval(
     a_overline = overline(a, delta)
     b_underline = underline(b, delta)
     b_overline = overline(b, delta)
-    print(f"\n{a_underline=}")
+    print(f"{a_underline=}")
     print(f"{a_overline=}")
     print(f"{b_underline=}")
     print(f"{b_overline=}")
@@ -45,12 +98,12 @@ def v_double_prime_for_interval(
 
     v_prime_a_under_b_over = _v_prime(segments, delta, a_underline, b_overline)
     v_prime_a_over_b_under = _v_prime(segments, delta, a_overline, b_underline)
-    print(f"{v_prime_a_under_b_over=}")
-    print(f"{v_prime_a_over_b_under=}")
+    print(f"{v_prime_a_under_b_over=}({a_underline=}, {b_overline=})")
+    print(f"{v_prime_a_over_b_under=}({a_overline=}, {b_underline=})")
 
     if a_overline - a >= b - b_underline:
         v_prime_a_under_b_under = _v_prime(segments, delta, a_underline, b_underline)
-        print(f"{v_prime_a_under_b_under=}")
+        print(f"{v_prime_a_under_b_under=}({a_underline=}, {b_underline=})")
         return (
             ((a_overline - a) - (b - b_underline)) / delta * v_prime_a_under_b_under
             + (b - b_underline) / delta * v_prime_a_under_b_over
@@ -58,7 +111,7 @@ def v_double_prime_for_interval(
         )
     elif a_overline - a <= b - b_underline:
         v_prime_a_over_b_over = _v_prime(segments, delta, a_overline, b_overline)
-        print(f"{v_prime_a_over_b_over=}")
+        print(f"{v_prime_a_over_b_over=}({a_overline=}, {b_overline=})")
         return (
             ((b - b_underline) - (a_overline - a)) / delta * v_prime_a_over_b_over
             + (a_overline - a) / delta * v_prime_a_under_b_over
@@ -69,10 +122,10 @@ def v_double_prime_for_interval(
 def overline(x, delta, epsilon=1e-10):
     assert 0 <= x <= 1
 
-    if x < delta:
+    if x < delta or x == 0:
         return delta
 
-    if x == 0 or x == 1:
+    if x == 1:
         return x
 
     v = math.ceil(x / delta) * delta
@@ -88,11 +141,11 @@ def overline(x, delta, epsilon=1e-10):
 def underline(x, delta, epsilon=1e-10):
     assert 0 <= x <= 1
 
-    if x < delta:
+    if x < delta or x == 0:
         return 0
 
-    if x == 0 or x == 1:
-        return x
+    if x == 1:
+        return x - epsilon
 
     # Check if x is an exact multiple of delta,
     # considering floating point precision issues
--- valuation_test.py
+++ valuation_test.py
@@ -1,12 +1,13 @@
 import pytest
 
-from treat_cake.algorithms.algorithm_test_utils import gen_flat_seg
+from treat_cake.algorithms.algorithm_test_utils import gen_flat_seg, gen_sloped_seg
 from treat_cake.valuation import (
     _v_prime,
     _v,
     overline,
     underline,
-    v_double_prime_for_interval,
+    _v_double_prime,
+    get_double_prime_for_interval,
 )
 
 EPSILON = 1e-1
@@ -19,23 +20,97 @@ def test_v():
     assert v == 1000
 
 
+def test_v_two_segs():
+    segs = [
+        gen_flat_seg(0, 50, 10),
+        gen_flat_seg(50, 100, 5),
+    ]
+
+    v = _v(segs, 0, 100)
+    assert v == 750
+
+
+def test_v_seesaw_like_gragh():
+    segs = [
+        gen_sloped_seg(
+            0,
+            50,
+            0,
+            10,
+        ),
+        gen_sloped_seg(50, 100, 5, 0),
+    ]
+    v = _v(segs, 0, 100)
+    assert v == 375
+
+
 def test_v_prime():
     segs = [gen_flat_seg(0, 100, 10)]
     v = _v_prime(segs, EPSILON, 0, 100)
     assert v == 510
 
 
+def test_v_prime_two_segs():
+    segs = [
+        gen_flat_seg(0, 50, 10),
+        gen_flat_seg(50, 100, 5),
+    ]
+
+    v = _v_prime(segs, 0.1, 0, 100)
+    assert v == 385
+
+
+def test_v_prime_seesaw_like_gragh():
+    segs = [
+        gen_sloped_seg(
+            0,
+            50,
+            0,
+            10,
+        ),
+        gen_sloped_seg(50, 100, 5, 0),
+    ]
+    v = _v_prime(segs, 0.1, 0, 100)
+    assert v == 197.5
+
+
 def test_v_double_prime_one_seg():
     segs = [gen_flat_seg(0, 1, 10)]
-    v = v_double_prime_for_interval(segs, EPSILON, 0, 1)
-    assert v == 5.1
+    v = _v_double_prime(segs, EPSILON, 0, 1)
+    assert v == pytest.approx(5.1, abs=TOLERANCE)
 
 
-def test_v_double_prime_two_seesaw_like_segs():
-    segs = [
-        gen_flat_seg(0, 1, 10),
-        gen_flat_seg(1, 2, 5),
+def test_v_double_prime_one_seg_by_interval():
+    segs = [gen_flat_seg(0, 1, 10)]
+    v = get_double_prime_for_interval(segs, EPSILON, 0, 1)
+    assert v == pytest.approx(5.1, abs=TOLERANCE)
+
+
+def test_v_double_prime_various_cases():
+    segs = [gen_flat_seg(0, 1, 10)]
+
+    test_cases = [
+        (0, 1, 5.1),
+        (0.4, 0.5, 1.54),
+        (0, 0.5, 3.06),
     ]
+    # test_cases = [(0, 1), (0.4, 5.5), (0, 0.5), (1.5, 4), (2, 2.5)]
+
+    for start, end, expected in test_cases:
+        print(f"\n======case {start}, {end}======")
+        result = get_double_prime_for_interval(segs, EPSILON, start, end)
+        assert result == pytest.approx(expected, abs=TOLERANCE)
+        print(
+            f"get_double_prime_for_interval(segments, epsilon, {start}, {end}) = {result}"
+        )
+
+
+# def test_v_double_prime_two_seesaw_like_segs():
+#     # TODO:Only focus on one segmentation.
+#     segs = [
+#         gen_flat_seg(0, 1, 10),
+#         gen_flat_seg(1, 2, 5),
+#     ]
 
 
 def test_overline():
@@ -57,6 +132,9 @@ def test_overline():
 
 
 def test_underline():
+    assert underline(0.4, 0.1) == pytest.approx(0.3, abs=TOLERANCE)
+    assert underline(0.5, 0.1) == pytest.approx(0.4, abs=TOLERANCE)
+
     assert underline(0.1, 0.05) == 0.05
 
     assert underline(0.02, 0.01) == 0.01
