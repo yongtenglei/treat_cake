3c7209a4ad54e79811916283fc44a0cc040f851b
yongtenglei <yongtengrey@outlook.com>
2024-07-15 17:36:37 +0100

    Added transform function between unit[0-1] interval and normal[1-cake_size] interval

--- type_helper.py
+++ type_helper.py
@@ -1,4 +1,4 @@
-from decimal import Decimal, getcontext
+from decimal import ROUND_HALF_UP, Decimal, getcontext
 
 getcontext().prec = 15
 
@@ -9,3 +9,21 @@ def to_decimal(value):
 
 def almost_equal(a: Decimal, b: Decimal, tolerance: Decimal) -> bool:
     return abs(a - b) <= tolerance
+
+
+def scale_to_unit(value: Decimal, cake_size: Decimal) -> Decimal:
+    """
+    Adjust value from [1, cake_size] to [0, 1]
+    """
+    return (to_decimal(value) - to_decimal(1)) / (to_decimal(cake_size) - to_decimal(1))
+
+
+def scale_back_from_unit(value: Decimal, cake_size: Decimal) -> Decimal:
+    """
+    Adjust value from [0, 1] back to [1, cake_size]
+    """
+    scaled_value = to_decimal(1) + to_decimal(value) * (
+        to_decimal(cake_size) - to_decimal(1)
+    )
+    # return to_decimal(scaled_value.to_integral_value(rounding=ROUND_HALF_UP))
+    return to_decimal(scaled_value.to_integral_value())
--- type_helper_test.py
+++ type_helper_test.py
@@ -1,6 +1,10 @@
 from decimal import Decimal
 
-from type_helper import to_decimal
+from pytest import approx
+
+from type_helper import scale_back_from_unit, scale_to_unit, to_decimal
+
+TOLERANCE = Decimal("1e-6")
 
 
 def test_to_decimal():
@@ -9,3 +13,45 @@ def test_to_decimal():
 
     a = Decimal(4)
     assert a == to_decimal(4)
+
+
+def test_scale_to_unit():
+    tolerance = to_decimal("1e-1")
+
+    cake_size = to_decimal(100)
+
+    a = to_decimal(1)
+    unit = scale_to_unit(a, cake_size)
+    expected = to_decimal(0)
+    assert unit == expected, f"expect: {expected}, got: {unit}"
+
+    b = to_decimal(cake_size)
+    unit = scale_to_unit(b, cake_size)
+    expected = to_decimal(1)
+    assert unit == expected, f"expect: {expected}, got: {unit}"
+
+    c = to_decimal(cake_size / 2)
+    unit = scale_to_unit(c, cake_size)
+    expected = to_decimal(0.5)
+    assert unit == approx(expected, rel=tolerance), f"expect: {expected}, got: {unit}"
+
+
+def test_scale_back_from_unit():
+    cake_size = to_decimal(100)
+
+    a = to_decimal(0)
+    origin = scale_back_from_unit(a, cake_size)
+    expected = to_decimal(1)
+    assert origin == expected, f"expect: {expected}, got: {origin}"
+
+    b = to_decimal(1)
+    origin = scale_back_from_unit(b, cake_size)
+    expected = to_decimal(cake_size)
+    assert origin == expected, f"expect: {expected}, got: {origin}"
+
+    c = to_decimal(0.5)
+    origin = scale_back_from_unit(c, cake_size)
+    expected = to_decimal(cake_size / 2)
+    assert origin == approx(
+        expected, rel=TOLERANCE
+    ), f"expect: {expected}, got: {origin}"
